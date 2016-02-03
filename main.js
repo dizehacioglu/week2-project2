0b767bbd0bc4cd2a6557eaef3bda92ada39bf17d
dizehacioglu <dh1272@nyu.edu>
2016-02-02 17:55:04 -0700

    finished exercises

--- main.js
+++ main.js
@@ -2,10 +2,22 @@
 
 // 1. Output each item in the following Array to your console:
 var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
+console.log('Q1');
+for(var i = 0; i < livingRoom.length; i++){
+  console.log(livingRoom[i]);
+}
 
 // 2. Using a loop, log numbers 22-33 in the console.
+console.log('Q2');
+for(var i = 22; i <= 33; i++){
+  console.log(i);
+}
 
 // 3. Using a similar loop, log numbers 75 to 100, only in increments of five.
+console.log('Q3');
+for(var i = 75; i < 101; i+=5){
+  console.log(i);
+}
 
 // 4. Write a while loop that logs "This is how a professional loops." to the console 5 times.
 //     Use this as an example:
@@ -18,25 +30,75 @@ var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
 //        }
 
 //        console.log(counter);
+console.log('Q4');
+var count = 0;
+while(count < 5){
+  console.log('This is how a professional loops');
+  count++;
+}
+
 
 // 5. Write a conditional statement to find the largest of the numbers in the array provided.
 var largestNum = [-5, -2, -6, 0, -1]
+console.log('Q5');
+for(var i = 0; i < largestNum.length-1; i++){
+  var num = largestNum[i];
+  if(num < largestNum[i+1]){
+    num = largestNum[i+1]
+  }
+}
+console.log('Largest num: ', num);
 
 // 6. Separately, use both a for loop and while loop to do the same thing.
 //     Print out the sentence "At home, I have _____ cats." Use the number from your counter to fill in the number.
 //     The numbers should range from 10 to 100, in increments of 25.
+console.log('Q6')
+for(var i = 10; i < 101; i+=25){
+  console.log('At home, I have ' + i + ' cats.')
+}
+
+var counter = 10;
+while(counter < 101){
+  console.log('At home, I have ' + counter + ' cats.');
+  counter += 25;
+}
 
 // 7. Given the following Array, console log 'Even' if the number is even, 'Even and greater than 10' if the
 //    number is even and greater than 10, and 'Odd' if the number is odd.
 //    HINT: Google 'remainder operator'
+console.log('Q7')
 var numArray = [2, 17, 9, 24, 8];
+for(var i = 0; i < numArray.length; i++){
+  if((numArray[i] % 2) == 0){
+    console.log('Even');
+  } else {
+    console.log('Odd');
+  }
+}
 
 // 8. Given the following Array, create variable primeArray with the value [2, 7, 17, 29, 41, 53, 67, 79, 97]
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,];
 var primeArray = [];
 
+console.log('Q8');
+for(var i = 0; i < primes.length; i+=3){
+  primeArray.push(primes[i])
+}
+console.log(primeArray);
+
 // 9. Prompt a user to pick either a number or a word. Create a script that alerts the user whether they chose a number or a word.
 // Hint: Google 'typeof'
+console.log('Q9');
+// var input = prompt('Pick either a number or a word');
+console.log(typeof input);
+switch(typeof input){
+  case 'number':
+    alert('You chose a number');
+    break;
+  case 'string':
+    alert('You chose a string');
+    break;
+}
 
 // 10. Write a loop that outputs the following to the console:
 // #
@@ -46,6 +108,10 @@ var primeArray = [];
 // #####
 // ######
 // #######
+console.log('Q10');
+for(var i = '#'; i.length < 8; i+="#"){
+  console.log(i);
+}
 
 
 // RO SHAM BO!
@@ -53,10 +119,16 @@ var primeArray = [];
 // randomly choose. You will use an alert to define the winner.
 
 // 11. Use a variable called "human" to prompt the user to type their choice.
+var human = prompt('Rock, paper, or scissors?');
+
 // 12. Define an array called "choices" that consists of "paper", "rock", and "scissors".
+var choices = ['paper', 'rock', 'scissors'];
+
 // 13. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
 //    For reference:
 //    Math.random gives you a random number between 0 and 1, which is different each time you call it.
+var computer = Math.random();
+
 // 14. Let's start our conditional statement. Start by reassigning the computer variable to "rock" if the random number
 //    falls between 0 and .33. Run the code until the console prints out "rock" instead of the number.
 //    In English, it will look like this:
@@ -64,9 +136,23 @@ var primeArray = [];
 //       if the number is between 0 and 33
 //          computer = "rock"
 //       log computer to the console
+if(computer >= 0 && computer <= 0.33){
+  computer = 'rock';
+}
+if(computer >= 0.34 && computer <= 0.66){
+  computer = 'paper';
+}
+if(computer >= 0.67 && computer <= 1) {
+  computer = 'scissors';
+}
+
 // 15. Now add the following conditionals to the same statement:
 //    If the random number is between .34 and .66, set the computer variable to "paper".
 //    If the random number is between .67 and 1, set the computer variable to "scissors". (Who the hell seriously chooses scissors first?)
+
+console.log('You played: ', human);
+console.log('Your opponent played: ', computer);
+
 // 16. Using both "human", begin another conditional statement. At this time, leave the statements blank.
 //    In English, it will look like this:
 //      if human is "rock"
@@ -75,10 +161,48 @@ var primeArray = [];
 //        leave this blank
 //      else
 //        leave this blank
+if(human == 'rock'){
+  if(human == computer){
+    console.log('It\'s a draw!');
+  }
+  if(computer == 'scissors'){
+    console.log('You win!')
+  }
+  if(computer == 'paper'){
+    console.log('You lose!')
+  }
+}
+
+if(human == 'paper'){
+  if(human == computer){
+    console.log('It\'s a draw!');
+  }
+  if(computer == 'scissors'){
+    console.log('You lose!')
+  }
+  if(computer == 'rock'){
+    console.log('You win!')
+  }
+}
+if(human == 'scissors') {
+  if(human == computer){
+    console.log('It\'s a draw!');
+  }
+  if(computer == 'paper'){
+    console.log('You win!')
+  }
+  if(computer == 'rock'){
+    console.log('You lose!')
+  }
+}
+
 // 17. Now inside of each condition, we need to compare the "human" variable to the "computer" variable,
 //    then print to the console who won the game.
 // 18. Give yourself a high five for completing your first javascript game!
 // 19. Use the game you made above and rework it using a switch statement.
+
+
+
 // 20. What happens if your user enters something other than "rock", "paper", or "scissors?". Change your default case
 //     to print a snarky message to the console if the input doesn't match any of the options.
 
@@ -91,13 +215,38 @@ var primeArray = [];
 //     Calling Math.floor on that number will truncate the decimal, and give you a
 //     random number within the bounds of your array. (In this case, our array will only contain two items.)
 // 22. Use a while loop to keep flipping the coin until you get tails.
+console.log('Q22');
+var side = ['heads', 'tails'];
+var count = 0;
+
+while(side[coin] == 'heads'){
+  console.log('Keep flipping...');
+  coin = Math.floor(Math.random() * 2);
+  if(side[coin] == 'tails'){
+    console.log('We got tails!')
+  }
+}
 
 // 23. FIZZ BUZZ
 // Write a program that uses console.log to print each number up to 100, with a couple exceptions...
 // If the number is divisible by 3, print "Fizz" instead of the number.
 // If the number is divisible by 5, print "Buzz" instead of the number.
 // If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
-
+console.log('Q23');
+for(var i = 0; i < 101; i++){
+  if(i % 3 == 0){
+    console.log('Fizz');
+  }
+  else if(i % 5 == 0){
+    console.log('Buzz');
+  }
+  else if( (i % 5 == 0) && (i % 3 == 0) ){
+    console.log('FizzBuzz')
+  }
+  else {
+    console.log(i);
+  }
+}
 
 // CHESS BOARD
 // 24. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
@@ -116,5 +265,25 @@ var primeArray = [];
 
 // When you have a program that generates this pattern, define a variable size = 8 and change the program
 // sso that it works for any size, outputting a grid of the given width and height.
+console.log('Q24');
+var s = '';
+for(var i = 0; i < 8; i++){
+  if(i % 2 == 0){
+    s = s.concat(' # # # #\n');
+  } else{
+    s = s.concat('# # # # \n');
+  }
+}
+console.log(s)
 
-
+console.log('Q24 Part 2');
+var size = 10;
+var s = '';
+for(var i = 0; i < size; i++){
+  if(i % 2 == 0){
+    s = s.concat(Array(size/2 + 1).join(' #') , '\n');
+  } else{
+    s = s.concat(Array(size/2 + 1).join('# ') , '\n');
+  }
+}
+console.log(s)
